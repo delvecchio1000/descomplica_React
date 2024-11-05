15a17490ab81c77f4f87a87ed4aa0f499b2b21f5
app-delvecchio <app.delvecchio@gmail.com>
2024-11-05 02:12:39 -0300

    tentativa 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tudo alterado professor. Aprendendo aos poucos.</h1>
+    </div>
+  );
+}
+
+export default App;
